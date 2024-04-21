ffef43dd29ee0b4a59e806e015022203bb7fb92a
BaiPiaoCN <wangrei1234567@163.com>
2024-04-21 19:32:57 +0800

    Initial commit

---
+++ src/main/java/org/baipiaocn/lobby_fly/LobbyFly.java
@@ -0,0 +1,17 @@
+package org.baipiaocn.lobby_fly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LobbyFly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
